5843b0cb89045f788274f1165889571a41f96aab
Karishma R <karishma08160@gmail.com>
2023-08-03 16:38:49 +0530

    completed

--- jss.js
+++ jss.js
@@ -1,1 +1,14 @@
 document.write("hey there people")
+window.alert("don't open this page")
+function h1(){
+      document.getElementById("intro").innerHTML="hey you!! THIS IS CHOCI"
+}
+function h2(){
+      document.getElementById("intro").innerHTML="hey you!! THIS IS SRS"
+}
+function h3(){
+      document.getElementById("intro").innerHTML="hey you!! THIS IS SUGAR"
+}
+function h4(){
+      document.getElementById("intro").innerHTML="hey you!! THIS IS ONION"
+}
